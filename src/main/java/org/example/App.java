6534a050b6b25f507f252fff3329ea37f1f02de5
MichalZurawski02 <90862515+MichalZurawski02@users.noreply.github.com>
2022-10-17 00:32:03 +0200

    update with gui (input/output service)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,26 @@
 package org.example;
+
 import javafx.application.Application;
-import javafx.collections.FXCollections;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.algorithms.bubbleSort;
+import org.example.algorithms.quickSort;
+import org.example.algorithms.sortAlgorithm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class App extends Application {
 
-    private static final int WINDOW_WIDTH = 1280;
-    private static final int WINDOW_HEIGHT = 720;
+    private static final int WINDOW_WIDTH = 400;
+    private static final int WINDOW_HEIGHT = 65;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -28,21 +33,57 @@ public class App extends Application {
 
         Button quickSortButton = new Button("Quick Sort");
         Button bubbleSortButton = new Button("Bubble Sort");
-        ArrayList<Button> sortButtons = new ArrayList<>();
-        sortButtons.add(quickSortButton);
-        sortButtons.add(bubbleSortButton);
-        ComboBox<Button> algo = new ComboBox<>(FXCollections.observableArrayList(sortButtons));
-        algo.setPromptText("Choose sorting algorithm");
 
         TextField input = new TextField("Enter integer");
-
+        ArrayList<Integer> arrayList = new ArrayList<>();
         Button addButton = new Button("Add");
-
-
+        Label label = new Label();
+        Label label2 = new Label();
         HBox top = new HBox();
-        top.getChildren().addAll(input,addButton ,algo);
+        VBox output = new VBox();
+        output.getChildren().addAll(label, label2);
+        top.getChildren().addAll(input,addButton,quickSortButton,bubbleSortButton);
 
         root.setTop(top);
+        root.setCenter(output);
+
+        addButton.setOnAction(e -> {
+            try {
+                arrayList.add(Integer.parseInt(input.getText()));
+                label.setText(arrayList.toString());
+            }
+            catch (Exception ex) {
+                label.setText("not an integer!");
+
+            }
+        });
+
+        quickSortButton.setOnAction(e -> Platform.runLater(() -> {
+
+            sortAlgorithm sort = new quickSort();
+           // System.out.println(arrayList);
+            //try {
+                Collections.sort(arrayList);
+                //arrayList.sort();
+                label2.setText(arrayList.toString());
+          //  } catch (InterruptedException ex) {
+              //  throw new RuntimeException(ex);
+           // }
+        }));
+
+        bubbleSortButton.setOnAction(e -> Platform.runLater(() -> {
+
+            sortAlgorithm sort = new bubbleSort();
+            //System.out.println(arrayList);
+            try {
+                sort.sort(arrayList);
+                label2.setText(arrayList.toString());
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }));
+
+        System.out.println();
 
         stage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
         stage.setMinHeight(WINDOW_HEIGHT);
